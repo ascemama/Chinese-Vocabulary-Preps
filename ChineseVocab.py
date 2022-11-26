0327e0619c2499d356f20624894aa51bd0af2168
Antoine Scemama <antoine.scemama@brainloop.com>
2022-11-26 19:01:47 +0800

    add re-review feature

--- ChineseVocab.py
+++ ChineseVocab.py
@@ -9,30 +9,18 @@ from docx.shared import Pt
 from docx.shared import Mm
 
 #which sheet and from which line should we start exporting
-#ExportSheetNb=9
-ExportSheetName="Sheet12"
-ExportFromLine=0
+ExportSheetNameList=["Sheet8","Sheet9","Sheet10","Sheet12"]
+ExportFromLine=105
 
-def retrieveDocJSON():
+ 
+def retrieveDoc():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('C:\\Users\\ascemama\\Documents\chinese-vocabulary-storage-d11d329ddf29.json', scope)
     client = gspread.authorize(creds)
-    sheet = client.open('ChineseVocab')
-    #sheet_instance = sheet.get_worksheet(ExportSheetNb)
-    sheet_instance=sheet.worksheet(ExportSheetName)
-    records_data = sheet_instance.get_all_records()
-    # test update data
-    #print(sheet_instance.cell(col=3,row=2))
-    #print(numbered_to_accented(records_data[2]["Pinyin"]))
-    #row=["a","b","c"]
-    #index=3
-    #sheet_instance.insert_row(row,index)
-    return records_data
+    chineseVocabDoc = client.open('ChineseVocab')
+    return chineseVocabDoc
 
-
- 
-
-def createWordDoc(DocJSON):
+def createNewWordDoc():
     document = Document()
     #set A4
     section = document.sections[0]
@@ -48,41 +36,86 @@ def createWordDoc(DocJSON):
     font = style.font
     font.name = 'Arial'
     font.size = Pt(11)  #100 characters per line
+    return document
+
+#for those words that were not learned well in the last weeks
+def addWordsToBeReReviewed(ChineseVocabDoc, newDoc):
     lineChinese=""
     lineTraduction=""
     isLastLine=False
-    DocJSONLen=len(DocJSON)
-    for idx in range(ExportFromLine,DocJSONLen):
-        pinyinLen=len(numbered_to_accented(DocJSON[idx]["Pinyin"]))
-        traductionLen=len(DocJSON[idx]["Traduction"])
+    sheets = ChineseVocabDoc.worksheets()
+    for sheet in sheets:
+        records_data = sheet.get_all_records()
+         
+        records_length=len(records_data)
+        for idx in range(0,records_length):
+            if(records_data[idx]["ToBeReviewed"]=="X"):
+                #print(records_data[idx]["Traduction"])
+                pinyinLen=len(numbered_to_accented(records_data[idx]["Pinyin"]))
+                traductionLen=len(records_data[idx]["Traduction"])
 
-        if(idx==(DocJSONLen-1)):
+                if(idx==(records_length-1)):
+                    isLastLine=True
+
+                if ((len(lineChinese)+pinyinLen > 40) or (len(lineTraduction)+traductionLen > 40)):
+                    padding=" "*(90-len(lineTraduction))
+                    newDoc.add_paragraph(lineTraduction+padding+lineChinese)
+                    lineChinese=numbered_to_accented(records_data[idx]["Pinyin"])
+                    lineTraduction=records_data[idx]["Traduction"]
+                else:
+                    if idx==0:
+                        lineChinese=numbered_to_accented(records_data[idx]["Pinyin"])
+                        lineTraduction=records_data[idx]["Traduction"]
+                    if (isLastLine):
+                        lineChinese=lineChinese+"/"+numbered_to_accented(records_data[idx]["Pinyin"])
+                        lineTraduction=lineTraduction+"/"+records_data[idx]["Traduction"]
+                    else:
+                        lineChinese=lineChinese+"/"+numbered_to_accented(records_data[idx]["Pinyin"])
+                        lineTraduction=lineTraduction+"/"+records_data[idx]["Traduction"]
+        
+                if(isLastLine):
+                    padding=" "*(90-len(lineTraduction))
+                    newDoc.add_paragraph(lineTraduction+padding+lineChinese)
+
+def addNewWords(ChineseVocabDoc, newDoc):
+    lineChinese=""
+    lineTraduction=""
+    isLastLine=False
+    sheet_instance=ChineseVocabDoc.worksheet(ExportSheetNameList[-1])
+    #print(sheet_instance)
+    records_data = sheet_instance.get_all_records()
+    records_length=len(records_data)
+    for idx in range(ExportFromLine,records_length):
+        pinyinLen=len(numbered_to_accented(records_data[idx]["Pinyin"]))
+        traductionLen=len(records_data[idx]["Traduction"])
+        #print("add new words "+records_data[idx]["Traduction"])
+        if(idx==(records_length-1)):
             isLastLine=True
 
         if ((len(lineChinese)+pinyinLen > 40) or (len(lineTraduction)+traductionLen > 40)):
             padding=" "*(90-len(lineTraduction))
-            document.add_paragraph(lineTraduction+padding+lineChinese)
-            lineChinese=numbered_to_accented(DocJSON[idx]["Pinyin"])
-            lineTraduction=DocJSON[idx]["Traduction"]
+            newDoc.add_paragraph(lineTraduction+padding+lineChinese)
+            lineChinese=numbered_to_accented(records_data[idx]["Pinyin"])
+            lineTraduction=records_data[idx]["Traduction"]
         else:
             if idx==0:
-                lineChinese=numbered_to_accented(DocJSON[idx]["Pinyin"])
-                lineTraduction=DocJSON[idx]["Traduction"]
+                lineChinese=numbered_to_accented(records_data[idx]["Pinyin"])
+                lineTraduction=records_data[idx]["Traduction"]
             if (isLastLine):
-                lineChinese=lineChinese+"/"+numbered_to_accented(DocJSON[idx]["Pinyin"])
-                lineTraduction=lineTraduction+"/"+DocJSON[idx]["Traduction"]
+                lineChinese=lineChinese+"/"+numbered_to_accented(records_data[idx]["Pinyin"])
+                lineTraduction=lineTraduction+"/"+records_data[idx]["Traduction"]
             else:
-                lineChinese=lineChinese+"/"+numbered_to_accented(DocJSON[idx]["Pinyin"])
-                lineTraduction=lineTraduction+"/"+DocJSON[idx]["Traduction"]
+                lineChinese=lineChinese+"/"+numbered_to_accented(records_data[idx]["Pinyin"])
+                lineTraduction=lineTraduction+"/"+records_data[idx]["Traduction"]
         
         if(isLastLine):
             padding=" "*(90-len(lineTraduction))
-            document.add_paragraph(lineTraduction+padding+lineChinese)
-    document.save('./vocab_'+str(date.today())+'.docx')
-
-
-
-
+            newDoc.add_paragraph(lineTraduction+padding+lineChinese)
+ 
 ### Main
-doc=retrieveDocJSON()
-createWordDoc(doc)
+
+chineseVocabDoc=retrieveDoc()
+newDoc=createNewWordDoc()
+addWordsToBeReReviewed(chineseVocabDoc,newDoc)
+addNewWords(chineseVocabDoc,newDoc)
+newDoc.save('./vocab_'+str(date.today())+'.docx')
