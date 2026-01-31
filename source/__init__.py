441ffa26e4f20ba929899add4f785e95ff5677f4
Antoine Scemama <antoine.scemama@brainloop.com>
2026-01-31 18:56:32 +0800

    update

--- source/__init__.py
+++ source/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
-from .gSheet import retrieveDoc,ExportSheetNameList, upload_to_google_sheet
+from .gSheetHelper import retrieveDoc,ExportSheetNameList, upload_to_google_sheet
 from .sqlLite import openDB, closeDB, updateDBWithNewWordFromNotebook,updateDBWithNewWordFromPleco
  
